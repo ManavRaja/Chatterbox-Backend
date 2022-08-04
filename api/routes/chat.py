32108996a060e88f4d8637b07fc964d5f9d2b39a
Manav Raja Vinotha <manav-the-dev@protonmail.com>
2022-08-04 13:09:01 -0500

    feat: Add route for users to send each other media such as images or videos.

--- api/routes/chat.py
+++ api/routes/chat.py
@@ -1,11 +1,10 @@
 from datetime import datetime, timezone
 
-from fastapi import APIRouter, Depends, Header, Form
+from fastapi import APIRouter, Depends, Header, Form, UploadFile
 
 from api.functions.auth import get_current_user, HTTPException
 from api.models import User
-from api.utils import get_db
-
+from api.utils import get_db, get_gridfs_db
 
 chat_router = APIRouter()
 
@@ -36,3 +35,38 @@ async def send_message(current_user_info: User = Depends(get_current_user), csrf
     await db.Chat.insert_one({"timestamp": datetime.now(timezone.utc), "sender": current_user_info.username,
                               "receiver": receiver_username, "type": "text", "message": message, "read": False})
     return {"msg": f"Your message to {receiver_username} was successfully sent."}
+
+
+@chat_router.post("/send-media")
+async def send_message(media: UploadFile, current_user_info: User = Depends(get_current_user), csrf_token: str = Header(...),
+                       receiver_username: str = Form(..., max_length=25), db=Depends(get_db),
+                       gridfs_db=Depends(get_gridfs_db)):
+    """
+    Route for users to send media such as images or videos to other users.
+    :param media: UploadFile object | the media being sent
+    :param current_user_info: user's info structured in User model | used to make sure user is authenticated and for
+    API to know which user is sending the media
+    :param csrf_token: HTTP header | prevents CSRF attack
+    :param receiver_username: str of Form Body | the user that the media is being sent to
+    :param db: AsyncIOMotorClient object | used to query db
+    :param gridfs_db: AsyncIOMotorGridFSBucket object | used to query gridfs
+    :return: 200 code & that the media was sent to th receiving user
+    :raises: 422 HTTPException if any defined HTTP parameter is missing & 409 HTTPException if the receiving user
+    doesn't exist or the receiving user's email isn't verified
+    """
+
+    receiver_username_document = await db.Users.find_one({"username": receiver_username})
+
+    if receiver_username_document is None:
+        raise HTTPException(status_code=409, detail="The receiving user doesn't exist.")
+    elif not receiver_username_document["verified"]:
+        raise HTTPException(status_code=409, detail="The receiving user's email isn't verified.")
+
+    grid_in = gridfs_db.open_upload_stream(media.filename, metadata={"media_type": f"chat_media_{media.content_type}",
+                                                                     "timestamp": datetime.now(timezone.utc),
+                                                                     "sender": current_user_info.username,
+                                                                     "receiver": receiver_username,
+                                                                     "read": False})
+    await grid_in.write(media.file)
+    await grid_in.close()
+    return {"msg": f"Your media to {receiver_username} was successfully sent."}
